6ad4fff0aa9870d4774f6140b3f384a17142f536
cc <15355498770@163.com>
2018-02-13 13:59:51 +0800

    initial

---
+++ static/js/calcjs.js
@@ -0,0 +1,22 @@
+document.write("hello")
+
+document.write("1"==1);
+document.write("1"===1);
+var x = 10;
+document.write(x>1?"x>1":"x<1");
+
+var arr = [1,3,45,6,76];
+for(var a in arr){
+    document.write(arr[a]+"<br>");
+}
+
+var x = 0;
+
+while(x<10){
+    x++;
+    document.write(x+"<br>");
+}
+
+document.write(" ------ - -- - -- - - - -- - - - ")
+
+
---
+++ static/js/myjs.js
@@ -0,0 +1,26 @@
+document.write("<h1>hello world</h1>");
+
+var i = 10;
+var j = 11;
+var m = i+j;
+document.write(m);
+
+var string1 = "hello";
+var string2 = "chen";
+document.write("<br>"+string1+" "+string2);
+
+var arr = [1,2,3,4];
+
+document.write("<br>"+arr[3]);
+var arr1 = new Array("hello","hello2");
+
+arr[0] = "hello update";
+
+document.write(arr[0]);
+
+var nuVar = null;
+document.write(nuVar);
+var unDefinedVar ;
+document.write(unDefinedVar);
+document.getElementById("pid").innerHTML = "hello world";
+document.write("....");//打印输出
